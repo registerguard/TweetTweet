559d97fff6f2afa4e63b69249872c692901676a7
Rob Denton <robdentonrg@gmail.com>
2015-05-13 16:24:21 -0700

    update for logs folder

--- tweet.py
+++ tweet.py
@@ -5,7 +5,7 @@ import logging, logging.handlers, urllib2, socket
 import feedparser, tweepy, bitly_api
 
 # --- LOGGING ---
-log_file_dir = '/Users/USER/PATH/TO/FILE/'
+log_file_dir = '/Users/USER/PATH/TO/TWEET/logs/'
 logger = logging.getLogger(__name__)
 # Set logging level
 logger.setLevel(logging.DEBUG)
@@ -48,43 +48,43 @@ feed = feedparser.parse(html)
 if feed.entries:
     #initiate new list, this will be populate with unique story IDs
     id_list = []
-    
+
     #populate list of ids
     for entry in feed.entries:
         id_list.append(entry.id)
-    
+
     #read past list of ids
     with open('/Users/USER/PATH/TO/FILE/tweet.txt', 'r') as f:
         file_data = f.read()
-    
+
     # Print out this stuff for testing purposes
     #logger.debug(id_list)
     #logger.debug type(id_list)
     #logger.debug(file_data)
-    
+
     #set loop var to count which id the loop is on
     loop = 0
-    
+
     # loop over items in list
     for single_id in id_list:
-        
+
         #logger.debug('single_id: ' + single_id)
-        
+
         # if item in list is not in data
         if single_id not in file_data:
             #logger.debug('not in read data: ' + single_id)
-            
+
             # set these vars
             feed_title = feed.entries[loop].title
             feed_url = feed.entries[loop].link
-            
+
             # --- TWEEPY ---
             # Credentials available at https://apps.twitter.com
             # Tweepy docs: http://tweepy.readthedocs.org/en/v3.3.0/getting_started.html
             auth = tweepy.OAuthHandler('CONSUMER_KEY', 'CONSUMER_SECRET')
             auth.set_access_token('ACCESS_TOKEN', 'ACCESS_TOKEN_SECRET')
             api = tweepy.API(auth)
-            
+
             # --- BITLY_API ---
             # Credentials available at https://bitly.com/a/create_oauth_app
             # Bitly_api docs: http://dev.bitly.com/get_started.html
@@ -99,12 +99,12 @@ if feed.entries:
                 # If there is an error, then fall back to the long URL that is provided from RSS, will still work
                 shorturl = feed_url
                 logger.debug(err)
-            
+
             # construct string to tweet
             # Example: This is a test headline http://rgne.ws/1E5UQmr
             tweet_text = feed_title + ' ' + shorturl
             #logger.debug (tweet_text)
-            
+
             try:
                 # Send the tweet
                 # Comment out the following line if you don't want to tweet during dev
@@ -112,22 +112,22 @@ if feed.entries:
                 logger.debug('Success! Tweet sent: ' + tweet_text)
             except tweepy.TweepError, err:
                 logger.debug(str(err.message[0]['message']) + ' ' + str(err.message[0]['code']))
-            
+
         # if item is already in the file then don't do anything
         else:
             logger.debug(single_id + ' is in the file.')
-        
+
         # add to loop as you loop through tweet.txt
         loop = loop + 1
         #logger.debug(loop)
-    
+
     # overwrite the file with the new ids
     with open('/Users/rdenton/Desktop/tweet.txt', 'w') as text_file:
         text_file.write('{}'.format(id_list))
-    
+
 #else if the feed does not have items then don't do anything
 else:
-    
+
     logger.debug('No entries')
-    
+
 # end of script
